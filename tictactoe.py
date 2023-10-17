46664a3283439c17a3457eb64e5318d0fe4648b6
BenjaminFalcon27 <benjamin.trolais.pro@gmail.com>
2023-10-17 15:27:34 +0200

    actions added' '

--- tictactoe.py
+++ tictactoe.py
@@ -22,14 +22,42 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    #Déclaration des compteurs de coups
+    x_count = 0
+    o_count = 0
+
+    #Si le board est vide, c'est au tour de X
+    if board == initial_state():
+        return X
+
+    #On compte le nombre de X et de O sur le board
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_count += 1
+            elif cell == O:
+                o_count += 1
+    
+    #Si le nombre de X est supérieur au nombre de O, c'est au tour de O et inversement
+    if x_count > o_count:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    
+    # actions = X, O, or EMPTY
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    
+    return possible_actions
 
 
 def result(board, action):
